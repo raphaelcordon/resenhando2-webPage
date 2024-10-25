ad771fc500bfc284ea6020837ac6ac81663281f7
raphaelcordon <cordon@outlook.com>
2024-10-25 10:27:24 +0200

    update store

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,6 @@
 import {configureStore} from '@reduxjs/toolkit'
-import userReducer from '../store/slices/userSlice.js'
 export default configureStore({
     reducer: {
-        user: userReducer,
+        
     },
 },)
