ce233f1ae15956bf041868defa15afd3fc136f88
raphaelcordon <cordon@outlook.com>
2024-10-25 13:02:27 +0200

    Renaming folders and preparing redux

--- src/routes/NotFound/index.jsx
+++
@@ -1,9 +0,0 @@
-const NotFound = () => {
-
-    return (
-        <>
-            <h1>NOT FOUND</h1>
-        </>
-    )
-}
-export default NotFound;
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import {configureStore} from '@reduxjs/toolkit'
+import userReducer from './slices/userReducer.js'
 export default configureStore({
     reducer: {
-        
+        user: userReducer,
     },
 },)
