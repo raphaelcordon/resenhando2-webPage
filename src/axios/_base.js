35074964d13aa114e1519d99abe791278dafb278
raphaelcordon <cordon@outlook.com>
2024-11-11 19:18:34 +0100

    fix API in axios

--- src/axios/_base.js
+++ src/axios/_base.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 const BASE_URL =
     window.location.hostname === "localhost"
         ? "http://localhost:5065/api/v1"
-        : "https://resenhando2-webapi-g7cjewefh8h9bta7.germanywestcentral-01.azurewebsites.net/";
+        : "https://resenhando2-webapi-g7cjewefh8h9bta7.germanywestcentral-01.azurewebsites.net/api/v1";
 
 const AxiosMotion = axios.create({
     baseURL: BASE_URL,
